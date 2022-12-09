db898811721f295f4656c84a6c958ceb38612fb6
Tommy Bidne <tbidne@protonmail.com>
2022-12-09 22:19:38 +1300

    Add unit tests for monad-callstack

---
+++ monad-callstack/test/unit/Main.hs
@@ -0,0 +1,61 @@
+module Main (main) where
+
+import Control.Exception (Exception, SomeException, throwIO, try)
+import Data.Functor ((<&>))
+import Data.String (IsString (fromString))
+import Effects.MonadCallStack
+  ( MonadCallStack (addCallStack, getCallStack, throwWithCallStack),
+    displayCallStack,
+  )
+import GHC.Stack (prettyCallStack)
+import System.FilePath ((</>))
+import Test.Tasty (TestTree, defaultMain, testGroup)
+import Test.Tasty.Golden (goldenVsStringDiff)
+
+main :: IO ()
+main =
+  defaultMain $
+    testGroup
+      "Unit Tests"
+      [ getsCallStack,
+        throwsCallStack,
+        addsCallStack
+      ]
+
+getsCallStack :: TestTree
+getsCallStack =
+  goldenVsStringDiff desc diff gpath $
+    fromString . prettyCallStack <$> getCallStack
+  where
+    desc = "Retrieves callstack"
+    gpath = goldenPath </> "get-callstack.golden"
+
+data Ex = MkEx
+  deriving stock (Eq, Show)
+  deriving anyclass (Exception)
+
+throwsCallStack :: TestTree
+throwsCallStack =
+  goldenVsStringDiff desc diff gpath $
+    try @SomeException (throwWithCallStack MkEx) <&> \case
+      Left e -> fromString $ displayCallStack e
+      Right _ -> "Error: did not catch expected exception."
+  where
+    desc = "Throws with callstack"
+    gpath = goldenPath </> "throw-callstack.golden"
+
+addsCallStack :: TestTree
+addsCallStack =
+  goldenVsStringDiff desc diff gpath $
+    try @SomeException (addCallStack $ throwIO MkEx) <&> \case
+      Left e -> fromString $ displayCallStack e
+      Right _ -> "Error: did not catch expected exception."
+  where
+    desc = "Adds callstack"
+    gpath = goldenPath </> "add-callstack.golden"
+
+goldenPath :: FilePath
+goldenPath = "test/unit/"
+
+diff :: FilePath -> FilePath -> [FilePath]
+diff ref new = ["diff", "-u", ref, new]
