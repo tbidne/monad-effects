8cb089603bf1173e480c69f4d67f2ded84e21b10
Tommy Bidne <tbidne@protonmail.com>
2023-01-18 23:02:30 +1300

    monad-exit: rename MonadEnv -> MonadExit

--- monad-exit/src/Effects/System/MonadExit.hs
+++ monad-exit/src/Effects/System/MonadExit.hs
@@ -1,9 +1,9 @@
--- | Provides the 'MonadEnv' typeclass.
+-- | Provides the 'MonadExit' typeclass.
 --
 -- @since 0.1
 module Effects.System.MonadExit
   ( -- * Effect
-    MonadEnv (..),
+    MonadExit (..),
     ExitCode (..),
 
     -- ** Functions
@@ -17,48 +17,50 @@ import Control.Monad.Trans.Class (MonadTrans (lift))
 import Control.Monad.Trans.Reader (ReaderT)
 import Data.ByteString.Char8 qualified as Char8
 import Effects.FileSystem.MonadHandleWriter
+  ( MonadHandleWriter (hPut),
+  )
 import GHC.Stack (HasCallStack)
 import System.Exit (ExitCode (..))
 import System.Exit qualified as Exit
 import System.IO qualified as IO
 
--- | Environment effects.
+-- | Exit effects.
 --
 -- @since 0.1
-class Monad m => MonadEnv m where
+class Monad m => MonadExit m where
   -- | Lifted 'Env.exitWith'.
   --
   -- @since 0.1
   exitWith :: HasCallStack => ExitCode -> m a
 
 -- | @since 0.1
-instance MonadEnv IO where
+instance MonadExit IO where
   exitWith = Exit.exitWith
   {-# INLINEABLE exitWith #-}
 
 -- | @since 0.1
-instance MonadEnv m => MonadEnv (ReaderT env m) where
+instance MonadExit m => MonadExit (ReaderT env m) where
   exitWith = lift . exitWith
   {-# INLINEABLE exitWith #-}
 
 -- | Lifted 'Exit.exitFailure'.
 --
 -- @since 0.1
-exitFailure :: MonadEnv m => m a
+exitFailure :: MonadExit m => m a
 exitFailure = exitWith (ExitFailure 1)
 {-# INLINEABLE exitFailure #-}
 
 -- | Lifted 'Exit.exitSuccess'.
 --
 -- @since 0.1
-exitSuccess :: MonadEnv m => m a
+exitSuccess :: MonadExit m => m a
 exitSuccess = exitWith ExitSuccess
 {-# INLINEABLE exitSuccess #-}
 
 -- | Lifted 'die'.
 --
 -- @since 0.1
-die :: (MonadEnv m, MonadHandleWriter m) => String -> m a
+die :: (MonadExit m, MonadHandleWriter m) => String -> m a
 die err = hPut IO.stderr err' *> exitFailure
   where
     err' = Char8.pack err
