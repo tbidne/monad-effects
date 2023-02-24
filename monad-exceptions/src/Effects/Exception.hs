35da84a23f54969ba97de587cae23e38d60e88f9
Tommy Bidne <tbidne@protonmail.com>
2023-02-25 11:28:14 +1300

    monad-exceptions: Improve docs

--- monad-exceptions/src/Effects/Exception.hs
+++ monad-exceptions/src/Effects/Exception.hs
@@ -608,21 +608,49 @@ catchesHandler handlers e = foldr tryHandler (throwM e) handlers
 -- These functions represent 'System.Exit'. 'System.Exit.die' can be found
 -- in "Effects.FileSystem.HandleWriter".
 
--- | Lifted 'Exit.exitFailure'.
+-- | The computation 'exitFailure' is equivalent to
+-- 'exitWith' @(@'ExitFailure' /exitfail/@)@,
+-- where /exitfail/ is implementation-dependent.
 --
 -- @since 0.1
 exitFailure :: (HAS_CALL_STACK) => (MonadThrow m) => m a
 exitFailure = exitWith (ExitFailure 1)
 {-# INLINEABLE exitFailure #-}
 
--- | Lifted 'Exit.exitSuccess'.
+-- | The computation 'exitSuccess' is equivalent to
+-- 'exitWith' 'ExitSuccess', It terminates the program
+-- successfully.
 --
 -- @since 0.1
 exitSuccess :: (HAS_CALL_STACK) => (MonadThrow m) => m a
 exitSuccess = exitWith ExitSuccess
 {-# INLINEABLE exitSuccess #-}
 
--- | Lifted 'System.Exit.exitWith'.
+-- | Computation 'exitWith' @code@ throws 'ExitCode' @code@.
+-- Normally this terminates the program, returning @code@ to the
+-- program's caller.
+--
+-- On program termination, the standard 'Handle's 'stdout' and
+-- 'stderr' are flushed automatically; any other buffered 'Handle's
+-- need to be flushed manually, otherwise the buffered data will be
+-- discarded.
+--
+-- A program that fails in any other way is treated as if it had
+-- called 'exitFailure'.
+-- A program that terminates successfully without calling 'exitWith'
+-- explicitly is treated as if it had called 'exitWith' 'ExitSuccess'.
+--
+-- As an 'ExitCode' is not an 'IOError', 'exitWith' bypasses
+-- the error handling in the 'IO' monad and cannot be intercepted by
+-- 'catch' from the "Prelude".  However it is a 'Control.Exception.SomeException', and can
+-- be caught using the functions of "Control.Exception".  This means
+-- that cleanup computations added with 'Control.Exception.bracket'
+-- (from "Control.Exception") are also executed properly on 'exitWith'.
+--
+-- Note: in GHC, 'exitWith' should be called from the main program
+-- thread in order to exit the process.  When called from another
+-- thread, 'exitWith' will throw an 'ExitException' as normal, but the
+-- exception will not cause the process itself to exit.
 --
 -- @since 0.1
 exitWith :: (HAS_CALL_STACK) => (MonadThrow m) => ExitCode -> m a
