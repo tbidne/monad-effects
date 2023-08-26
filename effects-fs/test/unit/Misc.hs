6fe7598e4237b7cc8aafe8ef66f538a6a1ad8f31
Tommy Bidne <tbidne@protonmail.com>
2023-08-26 19:11:17 +1200

    Add regression test against ByteString API

--- effects-fs/test/unit/Misc.hs
+++ effects-fs/test/unit/Misc.hs
@@ -1,14 +1,17 @@
 {-# LANGUAGE CPP #-}
+{-# LANGUAGE QuasiQuotes #-}
 
 module Misc (tests) where
 
 import Control.Monad.IO.Class (MonadIO (liftIO))
 import Data.ByteString (ByteString)
+import Data.ByteString qualified as BS
 import Data.Char qualified as Ch
 import Data.HashSet (HashSet)
 import Data.HashSet qualified as Set
-import Effects.Exception (MonadCatch, catchAny, displayException)
-import Effects.FileSystem.Utils (OsPath, (</>))
+import Effects.Exception (MonadCatch, catchAny, displayException, tryAny)
+import Effects.FileSystem.PathWriter qualified as WDir
+import Effects.FileSystem.Utils (OsPath, osp, (</>))
 import Effects.FileSystem.Utils qualified as Utils
 import Hedgehog (MonadGen, MonadTest, (===))
 import Hedgehog qualified as H
@@ -21,15 +24,19 @@ tests :: IO OsPath -> TestTree
 tests getTmpDir =
   testGroup
     "Misc"
-    [ readWriteRoundtrip getTmpDir
+    [ readWriteRoundtrip getTmpDir,
+      matchesBytestring getTmpDir
     ]
 
+-- | Tests that write/read is a roundtrip for valid paths.
 readWriteRoundtrip :: IO OsPath -> TestTree
 readWriteRoundtrip getTmpDir = testPropertyNamed desc "readWriteRoundtrip" $ do
   H.property $ do
-    tmpDir <- liftIO getTmpDir
+    tmpDir <- (</> [osp|readWriteRoundtrip|]) <$> liftIO getTmpDir
+    liftIO $ WDir.createDirectoryIfMissing True tmpDir
+    H.annotateShow tmpDir
 
-    fp <- H.forAll genFilePath
+    fp <- H.forAll genGoodFilePath
     contents <- H.forAll genFileContents
 
     os <- hcatch (Utils.encodeFpToValidOsThrowM fp)
@@ -46,6 +53,50 @@ readWriteRoundtrip getTmpDir = testPropertyNamed desc "readWriteRoundtrip" $ do
   where
     desc = "(read . write) round trips"
 
+-- | Tests that write/read success for ByteString's API implies success for
+-- Effect's API. This is essentially a regression test for our new OsPath
+-- API.
+matchesBytestring :: IO OsPath -> TestTree
+matchesBytestring getTmpDir = testPropertyNamed desc "matchesBytestring" $ do
+  H.property $ do
+    tmpDir <- (</> [osp|matchesBytestring|]) <$> liftIO getTmpDir
+    liftIO $ WDir.createDirectoryIfMissing True tmpDir
+    H.annotateShow tmpDir
+
+    fp <- H.forAll genAnyFilePath
+    contents <- H.forAll genFileContents
+
+    case Utils.encodeFpToValidOs fp of
+      Left _ -> H.label "Encoding FilePath failed"
+      Right os -> do
+        tmpDir' <- Utils.decodeOsToFpThrowM tmpDir
+        let filePath = tmpDir' `Utils.combineFilePaths` fp
+            osPath = tmpDir </> os
+
+        H.annotateShow osPath
+        H.annotateShow contents
+
+        -- The randomly generated path cannot be assumed to be valid, hence we
+        -- must check for failure. This allows us to check a larger domain than
+        -- matchesBytestring, at the cost of having to skip failures.
+        eBsResult <- tryAny $ liftIO $ do
+          BS.writeFile filePath contents
+          BS.readFile filePath
+
+        H.annotate "ByteString succeeded"
+
+        case eBsResult of
+          Left _ -> H.label "ByteString write failed"
+          Right bsResultContents -> do
+            H.label "ByteString write succeeded"
+            hcatch (Utils.writeBinaryFileIO osPath contents)
+            resultContents <- hcatch (Utils.readBinaryFileIO osPath)
+
+            bsResultContents === resultContents
+            contents === resultContents
+  where
+    desc = "ByteString.write/read succeeds => Effects.write/read succeeds"
+
 hcatch :: (MonadCatch m, MonadIO m, MonadTest m) => IO a -> m a
 hcatch m =
   liftIO m
@@ -58,8 +109,19 @@ genFileContents = Gen.utf8 range (Gen.filterT (/= '\0') Gen.unicode)
   where
     range = Range.linear 1 20
 
-genFilePath :: (MonadGen m) => m String
-genFilePath =
+-- | Generates a random unicode string without restrictions. The result
+-- is __not__ guaranteed to be a valid path on any platform.
+genAnyFilePath :: (MonadGen m) => m FilePath
+genAnyFilePath =
+  Gen.string range Gen.unicode
+  where
+    range = Range.linear 1 20
+
+-- | Generates a FilePath, excluding characters known to be invalid on each
+-- platform. The resulting FilePath should encode successfully as a valid
+-- OsPath.
+genGoodFilePath :: (MonadGen m) => m FilePath
+genGoodFilePath =
   Gen.string range genPathChar
   where
     range = Range.linear 1 20
@@ -99,7 +161,7 @@ charMapper :: Char -> Char
 -- https://en.wikipedia.org/wiki/UTF-8#Overlong_encodings
 genChar =
   let
-    -- s1 + s1 := Plane 0
+    -- s1 + s2 := Plane 0
     s1 =
       (55296, Gen.enum '\0' '\55295')
     s2 =
