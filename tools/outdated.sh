d2ddd99afcf48e6ba8002b59e750c206131b4937
Tommy Bidne <tbidne@protonmail.com>
2023-09-06 08:39:12 +1200

    Update text

--- tools/outdated.sh
+++ tools/outdated.sh
@@ -4,10 +4,19 @@ export LANG="C.UTF-8"
 
 export dirs="lib/effects-*"
 
+any_outdated=0
+
 for d in $dirs; do
   echo "$d"
   cd "$d"
-  cabal outdated
+  out=$(cabal outdated)
+  echo $out
   echo ""
   cd ../../
+
+  if [[ $out != "All dependencies are up to date." ]]; then
+    any_outdated=1
+  fi
 done
+
+exit $any_outdated
