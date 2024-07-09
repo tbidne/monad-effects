574de3d077e9bc06cc5ed0124f228b274ad91d0f
Tommy Bidne <tbidne@protonmail.com>
2024-07-10 11:27:25 +1200

    Add more lenient encoding tests

--- lib/effects-fs/test/unit/Utils.hs
+++ lib/effects-fs/test/unit/Utils.hs
@@ -1,9 +1,18 @@
 module Utils (tests) where
 
 import Control.Monad (void)
+import Data.Either (isRight)
 import Effects.FileSystem.Utils (OsPath)
 import Effects.FileSystem.Utils qualified as Utils
-import Hedgehog (Gen, evalNF, forAll)
+import Hedgehog
+  ( Gen,
+    PropertyT,
+    annotate,
+    annotateShow,
+    evalNF,
+    forAll,
+    (===),
+  )
 import Hedgehog qualified as H
 import Hedgehog.Gen qualified as Gen
 import Hedgehog.Range qualified as Range
@@ -16,14 +25,17 @@ tests =
   testGroup
     "Utils"
     [ testEncodeLenientTotal,
+      testEncodeLenientEqualsEncode,
       testEncodeValidLenientTotal,
-      testDecodeLenientTotal
+      testEncodeValidLenientEqualsEncode,
+      testDecodeLenientTotal,
+      testDecodeLenientEqualsDecode
     ]
 
 testEncodeLenientTotal :: TestTree
 testEncodeLenientTotal = testPropertyNamed desc "testEncodeLenientTotal" $ do
   H.property $ do
-    fp <- forAll genFilePath
+    fp <- forAll genSketchyFilePath
 
     let encoded = Utils.encodeFpToOsLenient fp
 
@@ -31,16 +43,46 @@ testEncodeLenientTotal = testPropertyNamed desc "testEncodeLenientTotal" $ do
   where
     desc = "encodeFpToOsLenient is total"
 
+testEncodeLenientEqualsEncode :: TestTree
+testEncodeLenientEqualsEncode = testPropertyNamed desc "testEncodeLenientEqualsEncode" $ do
+  H.property $ do
+    filePath <- forAll genFilePath
+
+    let eEncoded = Utils.encodeFpToOs filePath
+        encodedLenient = Utils.encodeFpToOsLenient filePath
+        encodeSuccess = isRight eEncoded
+
+    annotateShow encodedLenient
+
+    compareWithCoverage encodeSuccess eEncoded encodedLenient
+  where
+    desc = "encodeFpToOs == encodeOsToFpLenient for good paths"
+
 testEncodeValidLenientTotal :: TestTree
 testEncodeValidLenientTotal = testPropertyNamed desc "testEncodeValidLenientTotal" $ do
   H.property $ do
-    fp <- forAll genFilePath
+    fp <- forAll genSketchyFilePath
 
     let encoded = Utils.encodeFpToValidOsLenient fp
 
     void $ evalNF encoded
   where
     desc = "encodeFpToValidOsLenient is total"
+
+testEncodeValidLenientEqualsEncode :: TestTree
+testEncodeValidLenientEqualsEncode = testPropertyNamed desc "testEncodeValidLenientEqualsEncode" $ do
+  H.property $ do
+    filePath <- forAll genFilePath
+
+    let eEncoded = Utils.encodeFpToValidOs filePath
+        encodedLenient = Utils.encodeFpToValidOsLenient filePath
+        encodeSuccess = isRight eEncoded
+
+    annotateShow encodedLenient
+
+    compareWithCoverage encodeSuccess eEncoded encodedLenient
+  where
+    desc = "encodeFpToValidOs == encodeFpToValidOsLenient for good paths"
 
 testDecodeLenientTotal :: TestTree
 testDecodeLenientTotal = testPropertyNamed desc "testDecodeLenientTotal" $ do
@@ -53,6 +95,56 @@ testDecodeLenientTotal = testPropertyNamed desc "testDecodeLenientTotal" $ do
   where
     desc = "decodeOsToFpLenient is total"
 
+testDecodeLenientEqualsDecode :: TestTree
+testDecodeLenientEqualsDecode = testPropertyNamed desc "testDecodeLenientEqualsDecode" $ do
+  H.property $ do
+    osPath <- forAll genValidOsPath
+
+    let eDecoded = Utils.decodeOsToFp osPath
+        decodedLenient = Utils.decodeOsToFpLenient osPath
+        decodeSuccess = isRight eDecoded
+
+    annotate decodedLenient
+
+    compareWithCoverage decodeSuccess eDecoded decodedLenient
+  where
+    desc = "decodeOsToFp == decodeOsToFpLenient for good paths"
+
+-- NOTE: [Generating good paths]
+--
+-- We want to test "when paths are good ==> encode <=> encodeLenient",
+-- thus we want encoding to succeed. This is hard to gurantee, however,
+-- so in the case of an encode failure, we simply skip the test.
+--
+-- We want to ensure we are actually testing the right path most of
+-- the time, hence the coverage check.
+--
+-- The CoverPercentage is very high because in general there are very few
+-- generated paths that do not encode successfully. It isn't zero, though,
+-- so we cannot assume it always succeeds. We include the coverage check to
+-- ensure we are actually testing what we want most of the time.
+--
+-- We have tested that this actually make sense i.e. in a run with 100,000
+-- tests, only 3 failed, so there are in fact __some__ failures, but very few.
+--
+-- Note that hedghog's coverage checker performs rounding. In particular,
+-- 99.5+ will round to 100.
+compareWithCoverage ::
+  ( Eq a,
+    Show a
+  ) =>
+  Bool ->
+  Either e a ->
+  a ->
+  PropertyT IO ()
+compareWithCoverage isSuccess eResult resultLenient = do
+  H.cover 99 "Generation succeeded" isSuccess
+  case eResult of
+    Left _ -> H.label "Generated bad path"
+    Right result -> do
+      H.label "Generated good path"
+      result === resultLenient
+
 -- | The idea is, generate a possibly invalid OsPath s.t. generation is total
 -- but decoding might fail. Turns out this works i.e. if we replace
 -- decodeOsToFpLenient with unsafeDecodeOsToFp, then the test fails
@@ -60,9 +152,20 @@ testDecodeLenientTotal = testPropertyNamed desc "testDecodeLenientTotal" $ do
 -- doing something).
 genSketchyOsPath :: Gen OsPath
 genSketchyOsPath = do
-  str <- genFilePath
+  str <- genSketchyFilePath
   let osCharList = OsPath.unsafeFromChar <$> str
   pure $ OsPath.pack osCharList
 
+genSketchyFilePath :: Gen FilePath
+genSketchyFilePath = genSomeFilePath 0 Gen.unicodeAll
+
+genValidOsPath :: Gen OsPath
+genValidOsPath = Utils.unsafeEncodeFpToValidOs <$> genFilePath
+
 genFilePath :: Gen FilePath
-genFilePath = Gen.string (Range.linearFrom 0 0 50) Gen.unicodeAll
+genFilePath = genSomeFilePath 1 g
+  where
+    g = Gen.filter (/= '\NUL') Gen.unicode
+
+genSomeFilePath :: Int -> Gen Char -> Gen FilePath
+genSomeFilePath start = Gen.string (Range.linearFrom start start 50)
